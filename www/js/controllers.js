900e1383e1be05162ee2a3e995f398ee1e18f244
Fernando Varani <fervarani@gmail.com>
2016-01-27 18:36:02 -0200

    Some changes in home layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,13 +5,13 @@ angular.module('app.controllers', [])
     { src: 'img/inicio/1-carnaval.png', title: 'Destaque', url: '#/menu/programacao'},
     { src: 'img/inicio/2-esporte.jpg', title: 'Conheça Nazaré', url: '#/menu/conheca'},
     { src: 'img/inicio/3-restaurante.jpg', title: 'Restaurantes', url: '#/menu/alimentacao'},
-    { src: 'img/inicio/4-HOTEL.jpg', title: 'Hotéis/Pousadas e Marinas', url: '#/menu/conheca'},
-    { src: 'img/inicio/5-cidade.jpg', title: 'A Cidade', url: false},
-    { src: 'img/inicio/6-festas.jpg', title: 'Festejos e Calendário', url: false}
+    { src: 'img/inicio/4-HOTEL.jpg', title: 'Hotéis e Marinas', url: '#/menu/conheca'}
+    // { src: 'img/inicio/5-cidade.jpg', title: 'A Cidade', url: false},
+    // { src: 'img/inicio/6-festas.jpg', title: 'Calendário', url: false}
   ];
 
   $scope.loadImages = function() {
-      
+
   }
 })
 
